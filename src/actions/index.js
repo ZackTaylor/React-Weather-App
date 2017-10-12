8e5867a3f42bf9bd32eb35b4c2a57e405498e5bf
Zack Tylor <taylor.zackory@gmail.com>
2017-10-12 13:08:21 -0500

    create api call action generator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '7df95b3109f92fd0049142ff2937a36d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
